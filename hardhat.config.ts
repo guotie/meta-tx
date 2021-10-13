728a0f575a74aee16464252ced1e01a2a2b3303b
guotie <guotie.9@gmail.com>
2021-10-13 16:00:32 +0800

    add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,16 @@ import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import 'hardhat-local-networks-config-plugin';
+
+import {
+      eNetwork,
+      eEthereumNetwork,
+      ePolygonNetwork,
+      eXDaiNetwork,
+      NETWORKS_RPC_URL,
+      NETWORKS_DEFAULT_GAS,
+    } from './helpers/types';
 
 dotenv.config();
 
@@ -19,6 +29,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+const DEFAULT_BLOCK_GAS_LIMIT = 12450000;
+const DEFAULT_GAS_MUL = 5;
+const HARDFORK = 'istanbul';
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY || '';
+const MNEMONIC_PATH = "m/44'/60'/0'/0";
+const MNEMONIC = process.env.MNEMONIC || '';
+const UNLIMITED_BYTECODE_SIZE = process.env.UNLIMITED_BYTECODE_SIZE === 'true';
+const BUIDLEREVM_CHAINID = 31337;
+
+const getCommonNetworkConfig = (networkName: eNetwork, networkId: number) => ({
+  url: NETWORKS_RPC_URL[networkName],
+  hardfork: HARDFORK,
+  blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
+  gasMultiplier: DEFAULT_GAS_MUL,
+  gasPrice: NETWORKS_DEFAULT_GAS[networkName],
+  chainId: networkId,
+  accounts: {
+    mnemonic: MNEMONIC,
+    path: MNEMONIC_PATH,
+    initialIndex: 0,
+    count: 20,
+  },
+});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -26,16 +59,34 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    kovan: getCommonNetworkConfig(eEthereumNetwork.kovan, 42),
+    ropsten: getCommonNetworkConfig(eEthereumNetwork.ropsten, 3),
+    main: getCommonNetworkConfig(eEthereumNetwork.main, 1),
+    tenderlyMain: getCommonNetworkConfig(eEthereumNetwork.tenderlyMain, 3030),
+    hecotest: getCommonNetworkConfig(eEthereumNetwork.hecotest, 256),
+    heco: getCommonNetworkConfig(eEthereumNetwork.heco, 128),
+    bsc: getCommonNetworkConfig(eEthereumNetwork.bsc, 56),
+    bsctest: getCommonNetworkConfig(eEthereumNetwork.bsctest, 97),
+    matic: getCommonNetworkConfig(ePolygonNetwork.matic, 137),
+    mumbai: getCommonNetworkConfig(ePolygonNetwork.mumbai, 80001),
+    xdai: getCommonNetworkConfig(eXDaiNetwork.xdai, 100),
+    hardhat: {
+      chainId: BUIDLEREVM_CHAINID,
+      // saveDeployments: true,
+      allowUnlimitedContractSize: true,
+      mining: {
+        auto: true,
+        // interval: 0   // 使用 evm_mine 来手动挖矿
+      }
     },
   },
   // gasReporter: {
   //   enabled: process.env.REPORT_GAS !== undefined,
   //   currency: "USD",
   // },
+  mocha: {
+    timeout: 0,
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
